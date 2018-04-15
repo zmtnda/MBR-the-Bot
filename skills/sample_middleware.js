430042ba2799a05eab642a60a298f23d0905db06
Zin Tun <zitun@cisco.com>
2018-04-14 17:00:50 -0700

    middleware

--- skills/sample_middleware.js
+++ skills/sample_middleware.js
@@ -1,17 +1,12 @@
 module.exports = function(controller) {
 
-    controller.middleware.receive.use(function(bot, message, next) {
-    
-        bot.startRTM();
-        // listen for comma-separated 'hello-intent' or 'greeting-intent'
-        controller.hears('weather,help', 'direct_message', dialogflowMiddleware.hears, function(bot, message) {
-            bot.reply(message, 'Hello!');
-        });
-        console.log('RCVD:', message);
-        next();
-    
-    });
-    
+    controller.middleware.receive.use(dialogflowMiddleware.receive);
+ 
+    // listen for comma-separated 'hello-intent' or 'greeting-intent'
+    controller.hears('weather,help', 'direct_message', dialogflowMiddleware.hears, function(bot, message) {
+        console.log('SEND:', message);
+        bot.reply(message, 'Hello!');
+    });    
     //
     // controller.middleware.send.use(function(bot, message, next) {
     //
