0c42c12b1e14337173a5a9c9073646661e8a4a33
Zin Tun <zitun@cisco.com>
2018-04-14 17:28:01 -0700

    middleware

--- skills/sample_middleware.js
+++ skills/sample_middleware.js
@@ -1,22 +1,33 @@
+// Set up dialogFlow middleware
+var dialogflowMiddleware = require('botkit-middleware-dialogflow')({
+    token: process.env.dialog_token,
+});
 module.exports = function(controller) {
 
-    // Set up dialogFlow middleware
-    var dialogflowMiddleware = require('botkit-middleware-dialogflow')({
-        token: process.env.dialog_token,
-    });
-
-    controller.middleware.receive.use(dialogflowMiddleware.receive);
-    // listen for comma-separated 'hello-intent' or 'greeting-intent'
-    controller.hears('weather,help', 'direct_message', dialogflowMiddleware.hears, function(bot, message) {
-        bot.reply(message, 'Hello!');
-    });    
-    //
-    // controller.middleware.send.use(function(bot, message, next) {
-    //
+    // controller.middleware.receive.use(function(bot, message, next) {
+    
     //     // do something...
     //     console.log('SEND:', message);
     //     next();
-    //
+    
     // });
+    // controller.middleware.receive.use(dialogflowMiddleware.receive);
+    // // listen for comma-separated 'hello-intent' or 'greeting-intent'
+    // controller.hears('weather,help', 'direct_message', dialogflowMiddleware.hears, function(bot, message) {
+    //     bot.reply(message, 'Hello!');
+    // });    
+    //
+    controller.middleware.send.use(function(bot, message, next) {
+    
+        // do something...
+    
+        if (message.intent == 'help') {
+            message.text = 'Hello!!!';
+        }
+        console.log('SEND:', message);
+        bot.reply(message, 'Hello!');
+        next();
+    
+    });
 
 }
