68dc9b1d1508f6d92a1a96ac4cca3d2962fc4897
Zin Tun <zitun@cisco.com>
2018-04-15 18:52:10 -0700

    help fix

--- skills/help.js
+++ skills/help.js
@@ -4,17 +4,22 @@ module.exports = function(controller) {
     // This before middleware allows the help command to accept sub-thread names as a parameter
     // so users can say help to get the default thread, but help <subthread> will automatically
     // jump to that subthread (if it exists)
-    controller.studio.before('help', function(convo, next) {
+    // controller.studio.before('help', function(convo, next) {
 
-        // is there a parameter on the help command?
-        // if so, change topic.
-        if (matches = convo.source_message.text.match(/^help (.*)/i)) {
-            if (convo.hasThread(matches[1])) {
-                convo.gotoThread(matches[1]);
-            }
-        }
+    //     // is there a parameter on the help command?
+    //     // if so, change topic.
+    //     if (matches = convo.source_message.text.match(/^help (.*)/i)) {
+    //         if (convo.hasThread(matches[1])) {
+    //             convo.gotoThread(matches[1]);
+    //         }
+    //     }
 
-        next();
+    //     next();
+
+    // });
+    controller.hears(['help'], 'direct_message,direct_mention', function(bot, message) {
+
+        bot.reply('Ask any questions about the dashboards you have access to. For example: What is wtd America?');
 
     });
 
